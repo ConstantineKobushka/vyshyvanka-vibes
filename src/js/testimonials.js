2e9fb66a6e056719a003cb44d3ba8413749e6a32
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2024-05-26 16:25:29 +0200

    done testimonials
    with slider
    upd index for labriary
    upd html
    upd css styles

--- src/js/testimonials.js
+++ src/js/testimonials.js
@@ -1,1 +1,18 @@
-new Glide('.glide').mount();
+
+        const config = {
+            type: "carousel",
+            perView: 3,
+            breakpoints: {
+                1280: {
+                perView: 3
+                },
+                1279: {
+                perView: 2
+                },
+                767: {
+                perView: 1
+                }
+            }
+        };
+    new Glide(".glide", config).mount();        
+
