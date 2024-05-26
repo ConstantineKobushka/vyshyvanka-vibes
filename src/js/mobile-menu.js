085370e802960c2858443fa9044cf076a839d487
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2024-05-26 13:18:44 +0200

    header upd

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function () {
 
     const burgerIcon = document.getElementById('open-menu');
@@ -63,7 +62,7 @@ document.addEventListener('DOMContentLoaded', function () {
     closeButton.addEventListener('click', closeMenu);
 
 
-    
+
 
     const menuLinks = document.querySelectorAll('.menu-nav-link');
     menuLinks.forEach(function (link) {
@@ -79,49 +78,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 });
-
-
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
-
-  const toggleMenu = () => {
-    const anchors = mobileMenu.querySelectorAll('a[href*="#"]');
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-    mobileMenu.classList.toggle('is-open');
-    
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
-    
-    if(anchors.length === 0) return;
-    
-    if(!isMenuOpen) {
-      anchors.forEach(anchor => {
-        anchor.addEventListener("click", toggleMenu)
-      })
-      return;
-    }
-    
-    anchors.forEach(anchor => {
-        anchor.removeEventListener("click", toggleMenu)
-      })
-  };
-
-  openMenuBtn.addEventListener('click', toggleMenu);
-  closeMenuBtn.addEventListener('click', toggleMenu);
-
-  // Вказати брейкпоінт після якого повинна зачинятися
-  window.matchMedia('(min-width: 375px)').addEventListener('change', e => {
-    if (!e.matches) return;
-    mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
-  });
-})();
-
