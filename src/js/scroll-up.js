4afb7033bfc561fdab4fe2aa6c05621742a9ff59
Constantine Kobushka <kobushkaconstantine@gmail.com>
2024-05-29 20:09:16 +0300

    delet console.log

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -2,7 +2,6 @@ const offset = 100;
 const scrollUp = document.querySelector('.scroll-up');
 const scrollUpSvgPath = document.querySelector('.scroll-up-svg-path');
 const pathLength = scrollUpSvgPath.getTotalLength();
-console.log(pathLength);
 
 scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
 scrollUpSvgPath.style.transition = 'stroke-dashoffset 20ms';
@@ -12,7 +11,6 @@ const getTop = () => window.scrollY || document.documentElement.scrollTop;
 // updateDashoffset
 const updateDashoffset = () => {
   const height = document.documentElement.scrollHeight - window.innerHeight;
-  console.log(height);
   const dashoffset = pathLength - (getTop() * pathLength) / height;
   scrollUpSvgPath.style.strokeDashoffset = dashoffset;
 };
